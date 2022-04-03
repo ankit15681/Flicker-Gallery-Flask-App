e92a625e16868e70b59f2ac3a2f1d9faf1f76d0a
Ankit Meena <ankit15681@gmail.com>
2022-04-03 20:35:07 +0530

    Initial commit

---
+++ static/js/index.js
@@ -0,0 +1,30 @@
+let myHeaders = new Headers();
+let requestOptions = {
+  method: "GET",
+  headers: myHeaders,
+  redirect: "follow",
+};
+const el = document.getElementById('search');
+el.addEventListener('change',(e) =>fetchData(e))
+
+const fetchData =(e)=>{
+const container = document.getElementById("container");
+container.innerHTML = "";
+const term = e.target.value;
+fetch(
+  `https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=31d3b5c501c79e5ee5bcdd3fcd1c8ed0&text=${term}&format=json&nojsoncallback=1`,
+  requestOptions
+)
+  .then((response) => response.json())
+  .then((result) => {
+    const photosArry = result.photos.photo;
+    photosArry.map((photo) => {
+      const { id, server, secret } = photo;
+      let imageUrl = `https://live.staticflickr.com/${server}/${id}_${secret}_m.jpg`;
+      const content = `<img src=${imageUrl} />`;
+      container.innerHTML += content;
+    });
+  })
+  .catch((error) => console.log("error", error));
+
+}
